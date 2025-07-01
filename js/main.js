509e9c8adf5d8221e275eff4809375da91cebdfd
Alon3110 <6800688@gmail.com>
2025-07-01 16:47:36 +0300

    Increase the ball size and change the inner text

--- js/main.js
+++ js/main.js
@@ -1,5 +1,14 @@
 'use strict'
 
+var ballHeight = 100
+var ballWidth = 100
+
 function onBallClick() {
-    console.log('hi');
+    const elBall = document.querySelector('.ball')
+    ballHeight += 50
+    elBall.style.height = ballHeight + 50 + 'px'
+    ballWidth += 50
+    elBall.style.width = ballWidth + 50 + 'px'
+    const sum = ballHeight +ballWidth
+    elBall.innerText = sum
 }
