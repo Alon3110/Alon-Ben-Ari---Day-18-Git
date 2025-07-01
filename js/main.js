69e1b6ec373748ff88e11ca262907da52dae5c31
Alon3110 <6800688@gmail.com>
2025-07-01 17:42:59 +0300

    Add condition

--- js/main.js
+++ js/main.js
@@ -1,14 +1,24 @@
 'use strict'
 
-var ballHeight = 100
-var ballWidth = 100
+var ballHeight = 50
+var ballWidth = 50
 
 function onBallClick() {
+
     const elBall = document.querySelector('.ball')
     ballHeight += 50
-    elBall.style.height = ballHeight + 50 + 'px'
     ballWidth += 50
+    const sum = ballHeight + ballWidth
+
+    elBall.style.height = ballHeight + 50 + 'px'
     elBall.style.width = ballWidth + 50 + 'px'
-    const sum = ballHeight +ballWidth
     elBall.innerText = sum
+
+    if (sum > 400) {
+        ballHeight = 50
+        ballWidth = 50
+        elBall.style.height = 100 + 'px'
+        elBall.style.width = 100 + 'px'
+        elBall.innerText = '100'
+    }
 }
